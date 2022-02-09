f3ae33ee09d0f548d6fefddaaf35dbd2a64df50e
piotr25691 <71488561+piotr25691@users.noreply.github.com>
2022-02-09 12:49:24 +0100

    fuck you kernel

--- trolley-arch.sh
+++ trolley-arch.sh
@@ -28,7 +28,7 @@ else
     FILES=() \
     HOOKS=(base udev autodetect modconf block filesystems fsck)" > /mnt/etc/mkinitcpio.conf
     # install needed packages
-    pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd iwd networkmanager btrfs-progs
+    pacstrap /mnt base base-devel linux-hardened linux-firmware nano dhcpcd iwd networkmanager btrfs-progs
     echo $HOSTNAME >> /mnt/etc/hostname
     # generate a fstab and go to chroot
     genfstab -U /mnt >> /mnt/etc/fstab
