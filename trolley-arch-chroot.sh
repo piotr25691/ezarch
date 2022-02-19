036cb025fedda8a51dfd3a073d4484a8459cf9d5
piotr25691 <71488561+piotr25691@users.noreply.github.com>
2022-02-19 10:04:25 +0100

    sudo set up and some commenting

--- trolley-arch-chroot.sh
+++ trolley-arch-chroot.sh
@@ -5,19 +5,24 @@ then
 else
     DEV=/dev/sda
 fi
-pacman --noconfirm -Sy grub
 # install grub
+pacman --noconfirm -Sy grub
 grub-install --target=i386-pc --force $DEV
 grub-mkconfig -o /boot/grub/grub.cfg
+# set up root password
 read -p "Please enter your root password: " ROOTPASS
 echo "root:$ROOTPASS" | chpasswd
+# set up normal user
 read -p "Enter the username of your user: " USERNAME
 useradd -m $USERNAME
 usermod -aG wheel $USERNAME
 read -p "Please enter your user password: " USERPASS
 echo "$USERNAME:$USERPASS" | chpasswd
-# remove all the useless stuf
+# set up sudo
+sed -i '/%wheel ALL=(ALL:ALL) ALL/s/^# //g' /etc/sudoers
+# delete grub after config
 pacman --noconfirm -R grub
+# enable the network
 systemctl enable dhcpcd
 systemctl enable iwd
 systemctl enable NetworkManager
