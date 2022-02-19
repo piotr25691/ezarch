3c22533dcfc3faed029763763a449197e95bd193
piotr25691 <71488561+piotr25691@users.noreply.github.com>
2022-02-19 09:35:00 +0100

    added normal user

--- trolley-arch-chroot.sh
+++ trolley-arch-chroot.sh
@@ -9,7 +9,13 @@ pacman --noconfirm -Sy grub
 # install grub
 grub-install --target=i386-pc --force $DEV
 grub-mkconfig -o /boot/grub/grub.cfg
+echo "Please enter your root password below."
 passwd
+read -p "Enter the username of your user: " USERNAME
+useradd $USERNAME
+usermod -aG wheel $USERNAME
+echo "Please enter your user password below."
+passwd $USERNAME
 # remove all the useless stuf
 pacman --noconfirm -R grub
 systemctl enable dhcpcd
