6577200a48746882f9bca62d004d2982738ee91d
lun4h <71488561+piotr25691@users.noreply.github.com>
2023-02-24 10:22:01 +0100

    update notes
    it won't work on NVMe either

--- ezarch.sh
+++ ezarch.sh
@@ -4,13 +4,13 @@
 #
 # This script consists of two parts:
 # - ezarch.sh: installs the base system to your hard disk
-# - ezarch-chroot.sh: does post-setup of your newly installed arch system
+# - ezarch-chroot.sh: does post-setup of your newly installed Arch system
 #
 # To get started, run ezarch.sh
 # Be aware that this will wipe your drive clean, so check if you got backups,
 # or that you don't need stuff currently on your hard disk.
 #
-# This script requires an IDE, SCSI or SATA disk, MMC storage is not supported.
+# This script requires an IDE, SCSI or SATA disk, MMC/NVMe storage is not supported.
 #
 # Enjoy using Arch btw!
 
